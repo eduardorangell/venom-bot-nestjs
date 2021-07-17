5b632ade6f2cd94d26febbe6ca34e300b81a8319
Eduardo Rangell <rangellferreira@gmail.com>
2021-07-17 19:34:01 -0300

    feature: max size limit to file upload

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { urlencoded, json } from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import env from '@environments';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug'],
+  });
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.useStaticAssets(join(__dirname, '..', env().FILE_UPLOAD_DIR));
+  await app.listen(env().PORT);
 }
 bootstrap();
