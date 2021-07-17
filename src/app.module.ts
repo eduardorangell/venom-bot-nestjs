07c7b3129a4aa6daf5d7682b19f9147639e052dd
Eduardo Rangell <rangellferreira@gmail.com>
2021-07-17 19:35:20 -0300

    chore: set some imports to multer and config work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import env from '@environments';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [env], cache: true }),
+    MulterModule.register({
+      dest: env().FILE_UPLOAD_DIR,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
