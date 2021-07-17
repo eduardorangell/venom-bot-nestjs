aa5057f92125c312930235aca029d69f6d219a41
Eduardo Rangell <rangellferreira@gmail.com>
2021-07-17 19:31:23 -0300

    feature: file upload

---
+++ src/helpers/multer.config.ts
@@ -0,0 +1,51 @@
+import { extname } from 'path';
+import { existsSync, mkdirSync } from 'fs';
+import { diskStorage } from 'multer';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import env from '@environments';
+
+// Multer configuration
+export const multerConfig = {
+  dest: env().FILE_UPLOAD_DIR,
+};
+
+// Multer upload options
+export const multerOptions = {
+  // Enable file size limits
+  limits: {
+    fileSize: env().MAX_FILE_SIZE,
+  },
+  // Check the mimetypes to allow for upload
+  fileFilter: (req: any, file: any, cb: any) => {
+    if (file.mimetype.match(RegExp(env().ALLOWED_FILETYPES))) {
+      // Allow storage of file
+      cb(null, true);
+    } else {
+      // Reject file
+      cb(
+        new HttpException(
+          `Unsupported file type ${extname(file.originalname)}`,
+          HttpStatus.BAD_REQUEST,
+        ),
+        false,
+      );
+    }
+  },
+  // Storage properties
+  storage: diskStorage({
+    // Destination storage path details
+    destination: (req: any, file: any, cb: any) => {
+      const uploadPath = multerConfig.dest;
+      // Create folder if doesn't exist
+      if (!existsSync(uploadPath)) {
+        mkdirSync(uploadPath);
+      }
+      cb(null, uploadPath);
+    },
+    // File modification details
+    filename: (req: any, file: any, cb: any) => {
+      // Calling the callback passing the random name generated with the original extension name
+      cb(null, `${Date.now()}${extname(file.originalname)}`);
+    },
+  }),
+};
